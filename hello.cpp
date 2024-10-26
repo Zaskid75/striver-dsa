7e53ec52ab6c1ea851ada14a3f2163cbaf917b57
unknown <azshawon75@gmail.com>
2024-10-26 15:06:21 +0600

    dsa striver

---
+++ hello.cpp
@@ -0,0 +1,131 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int main(){
+// int x,y;
+// cin>>x>>y;
+// cout<<"value of x= "<<x<<" and y= "<<y;
+
+// string str;
+// getline(cin,str);
+// cout<<str;
+
+// char ch;
+// cin>>ch;
+// cout<<ch;
+
+// int,long,long long,float,double
+// string and getline
+// char
+
+// int age;
+// cin>>age;
+// if(age>=18){
+//   cout<<"you are an adult";
+// }
+// else{
+//   cout<<"you are not an adult";
+// }
+
+// exam result
+
+// int marks;
+// cin>>marks;
+// if(marks<25){
+//   cout<<"F";
+// }
+// else if(marks<=44){
+//   cout<<"E";
+// }
+// else if(marks<=49){
+//   cout<<"D";
+// }
+// else if(marks<=59){
+//   cout<<"C";
+// }
+// else if(marks<=79){
+//   cout<<"B";
+// }
+// else if(marks<=100){
+//   cout<<"A";
+// }
+// return 0;
+
+// int age;
+// cin>>age;
+// if(age<18){
+//   cout<<"not eligible for job";
+// }
+// else if(age<=54){
+//   cout<<"eligible for job";
+// }
+// else if(age<=57){
+//   cout<<"eligible for job but ret soon";
+// }
+// else{
+//   cout<<"retirement time";
+// }
+// return 0;
+
+
+// now discuss about loops
+// for (int i = 1; i <= 10; i++)
+// {
+//    cout<<"shawon\n";
+// }
+
+// int i=1;
+// while (i<=5)
+// {
+//   cout<<"sHaWon\n";
+//   i++;
+// }
+
+// functions-->
+//  void-->its does not return anything
+//  parameterised
+//  return 
+//  non parameterised
+
+
+// void sum(int x,int y){
+//    int num3=x+y;
+//    cout<<num3;
+// }
+
+// void doSomething(string s){
+//     s[0]='x';
+//     cout<<s<<endl;
+// }
+
+
+
+int main(){
+
+// int num1,num2;
+// cin>>num1>>num2;
+// sum(num1,num2);
+
+// pass by value
+// string s="sHaWon";
+// doSomething(s);
+// cout<<s<<endl;
+
+// pass by reference -->it changes the main value (&)
+// array always goes in reference
+
+// time complexity in worst case is
+// big O notation
+
+// space complexity aux space+inp space
+
+
+
+
+
+
+
+
+
+return 0;
+}
